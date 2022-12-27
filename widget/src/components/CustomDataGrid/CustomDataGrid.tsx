6632356217da7ed8a6027dea6aaaa5d0aa9a3040
ranatallal09 <55754167+ranatallal09@users.noreply.github.com>
2022-12-27 08:43:25 +0500

    responsiveness fixed

--- widget/src/components/CustomDataGrid/CustomDataGrid.tsx
+++ widget/src/components/CustomDataGrid/CustomDataGrid.tsx
@@ -1,12 +1,13 @@
 import React, {useMemo} from 'react';
-import { DataGrid,GridColDef,GridToolbarContainer} from '@mui/x-data-grid';
+import {Grid, useMediaQuery, useTheme} from '@mui/material';
+import {DataGrid, GridColDef, GridToolbarContainer} from '@mui/x-data-grid';
 import {createBrowserHistory} from "history";
 import {columnsConfigs} from "../../configs";
 import {useAPI} from "../../api";
 
-const CustomToolbar=()=> {
+const CustomToolbar = () => {
     return (
-        <GridToolbarContainer style={{ justifyContent: 'center' }}>
+        <GridToolbarContainer style={{justifyContent: 'center'}}>
             <p className={"heading"}>
                 Hey Iam a DataGrid Widget!
             </p>
@@ -14,21 +15,43 @@ const CustomToolbar=()=> {
     );
 }
 
-export const CustomDataGrid=({history=createBrowserHistory(),widget=false})=> {
-    const {loading,data}=useAPI(process.env.REACT_APP_DATA_API);
-    const columns=useMemo<GridColDef[]>(()=>columnsConfigs.map((column)=>{
+const MobileRow = ({rowData}: { rowData: any }) => {
+    return (
+        <Grid sx={{borderBottom: '1px solid black', p: '0.5rem 0 0.5rem 0'}} container justifyContent={'center'}
+              alignItems={'center'}>
+            {columnsConfigs.map((column, index) =>
+                <Grid container item key={index}>
+                    <Grid sx={{fontWeight: 'bold'}} item xs={6} sm={6}>{column.label}</Grid>
+                    <Grid item xs={6} sm={6}>{rowData[column.key]}</Grid>
+                </Grid>
+            )}
+        </Grid>
+    )
+}
+
+export const CustomDataGrid = ({history = createBrowserHistory(), widget = false}) => {
+    const {loading, data} = useAPI(process.env.REACT_APP_DATA_API);
+    const theme = useTheme();
+    const matches = useMediaQuery(theme.breakpoints.down('sm'));
+
+    const columns = useMemo<GridColDef[]>(() => columnsConfigs.map((column) => {
         return {
-            field:column.key,
-            headerName:column.label,
-            type:column.type,
-            flex:1,
-            valueGetter: ({ value }) => column.type==='date'? value && new Date(value):value,
+            field: column.key,
+            headerName: column.label,
+            type: column.type,
+            flex: 1,
+            hide: matches,
+            valueGetter: ({value}) => column.type === 'date' ? value && new Date(value) : value,
         }
-    }),[])
+    }), [matches])
 
     return (
-           <div style={{ height:'100%', width: '100%' }}>
-              <DataGrid components={{Toolbar: CustomToolbar}} loading={loading} rows={data} columns={columns} />
-           </div>
+        <div style={{height: '100%', width: '100%'}}>
+            <DataGrid disableVirtualization={matches} components={{
+                Toolbar: CustomToolbar, ...matches && {
+                    Row: (rowData) => <MobileRow rowData={rowData.row}/>
+                }
+            }} loading={loading} rows={data} columns={columns}/>
+        </div>
     );
 };
